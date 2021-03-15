e84b54b8d0cc1a5c9c237cfe4ce1c4ce44d12ec0
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-03-15 17:54:09 +0000

    add the file  _printf.c

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+* _printf - function that print characters for condicional character
+* @format: char pointer
+*
+* Return: integer equal to lenght the print
+*/
+
+
+int _printf(const char *format, ...)
+{
+	va_list pa;
+	char *p;
+	int num = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(pa, format);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			switch (*++p)
+			{
+				case 's':
+					num = num + print_string(pa);
+					break;
+				case 'c':
+					num = num + print_character(pa);
+					break;
+				case '%':
+					num = num + 1;
+					_putchar('%');
+					break;
+				case '\0':
+					return (-1);
+				case 'i':
+				case 'd':
+					num = num + print_integer(pa);
+					break;
+				default:
+					_putchar('%'), _putchar(*p), num = num + 2;
+			}
+		}
+		else
+			_putchar(*p), num++;
+	}
+	va_end(pa);
+	return (num);
+}
